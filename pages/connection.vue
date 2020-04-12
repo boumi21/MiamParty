537d7243830f5323c9a09e1f9d0ada624ce89a12
boumi21 <glorkpatate@yahoo.be>
2020-04-12 12:19:23 +0200

    Ajout page de connexion
    - Ajout de la page de connexion avec les champs email et le mdp à remplir pour se connecter

--- pages/connection.vue
+++ pages/connection.vue
@@ -1,46 +1,58 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        Connexion
-      </h1>
-      
-      <div class="links">
-      
-      </div>
-    </div>
+  <div>
+    <b-form @submit="onSubmit" v-if="show">
+      <b-form-group
+        id="input-group-email"
+        label="Courrier électronique:"
+        label-for="input-email"
+      >
+        <b-form-input
+          id="input-email"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="Votre mel"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-mdp" label="Mot de passe:" label-for="input-mdp">
+        <b-form-input
+          id="input-mdp"
+          v-model="form.mdp"
+          type="password"
+          required
+          placeholder="Votre mot de passe"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Connexion</b-button>
+      <!-- <b-button type="reset" variant="danger">Reset</b-button> -->
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
-  components: {
-    Logo
-  }
+  data() {
+      return {
+        form: {
+          email: '',
+          mdp: ''
+        },
+        show: true
+      }
+    },
+    methods: {
+      onSubmit(evt) {
+        evt.preventDefault()
+        alert(JSON.stringify(this.form))
+      }
+    }
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 50px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-</style>
