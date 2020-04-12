4f914e53f180f1892f5a8d44168d65336b8b4bcb
boumi21 <glorkpatate@yahoo.be>
2020-04-12 19:25:03 +0200

    ajout d'un formulaire dans l'index

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,38 @@
       <h3 class="info">
         Connectez-vous pour découvrir toutes nos fonctionnalités.
       </h3>
+      <div>
+    <b-form @submit="onSubmit" v-if="show">
+      
+
+      <b-form-group id="input-group-pseudo" label="Votre pseudo:" label-for="input-pseudo">
+        <b-form-input
+          id="input-pseudo"
+          v-model="form.pseudo"
+          required
+          placeholder="Entrez votre pseudo"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-mdp" label="Mot de passe:" label-for="input-mdp">
+        <b-form-input
+          id="input-mdp"
+          v-model="form.mdp"
+          type="password"
+          required
+          placeholder="Entrez votre mot de passe"
+        ></b-form-input>
+      </b-form-group>
+
+      
+
+      <b-button type="submit" variant="primary">Je m'inscris</b-button>
+      <!-- <b-button type="reset" variant="danger">Reset</b-button> -->
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
+  </div>
       <div class="links">
         <a
           href="/connection"
@@ -35,11 +67,30 @@ var Database = require('~/server/database/Database.js');
 
 //Database.request("SELECT * from TR_COOKING_LEVEL;");
 
-export default {
-  components: {
+
+
+var particulier = require('~/server/dao/particulier.js');
+
+  export default {
+    components: {
     Logo
+  },
+    data() {
+      return {
+        form: {
+          pseudo: '',
+          mdp: ''
+        },
+        show: true
+      }
+    },
+    methods: {
+      async onSubmit(registrationInfo) {
+          console.log("lol");
+        let user = await particulier.addUser(registrationInfo);
+      },
+    }
   }
-}
 
 </script>
 
