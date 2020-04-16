5261dbbedfe16afff5ab9b3a88ab09bcdd0be463
camille-bonnetain <camille.bonnetain@outlook.com>
2020-04-16 16:20:10 +0200

    modif requête création compte

--- server/dao/Particular.js
+++ server/dao/Particular.js
@@ -6,13 +6,24 @@ exports.signUp = function(registrationInfo) {
     var salt = Password.getSalt(64);
     var password = Password.hashPassword(salt + registrationInfo.password);
 
-    var request = 'INSERT INTO Account SET'
-                + ' id_account_type = ' + 0
+    var getAccountType = 'SELECT * FROM'
+                       + 'account_type'
+                       + ' WHERE'
+                       + ' account_type.description = "Particular"';
+
+    var result = Database.sendRequest(getAccountType);
+    var accountType = result[0].account_type;
+    
+
+
+
+    var insertParticular = 'INSERT INTO Account SET'
+                + ' id_account_type = ' + accountType
                 + ', username = ' + registrationInfo.username
                 + ', password = ' + password
                 + ', salt = ' + salt
                 + ', email = ' + registrationInfo.email
                 + ', creation_date = NOW()'
-                + ', last_access = NOW()'
-    Database.sendRequest(request);
+                + ', last_access = NOW()';
+    Database.sendRequest(insertParticular);
 }
