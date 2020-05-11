249d9621c0af7dfa0a8c392587148e0c80276b2e
boumi21 <glorkpatate@yahoo.be>
2020-05-11 21:00:58 +0200

    Beautification create et search
    Page create et search un peu plus belles même si c'est pas la folie
    
    -> Possibilité de changer le layout de search en utilisant les cards façon maçonnerie https://bootstrap-vue.org/docs/components/card#cards
    Il faut remplacer le "deck" par "columns", supprimmer le "row" et supprimmer le class dans le div qui contient le for

--- client/pages/party/create.vue
+++ client/pages/party/create.vue
@@ -5,171 +5,184 @@
       <h2 class="subtitle">Créez une soirée en toute simplicité</h2>
 
       <b-form @submit="onSubmit" id="form_Create_Part" v-if="show" method="post">
-
-        <hr>
-
-        <h4>Informations sur votre soirée</h4>
-
-        <b-form-group
-          id="input-group-name"
-          label="Quel sera le nom de votre soirée ?"
-          label-for="input-name"
+        <hr />
+        <b-card
+          header="1) Informations sur votre soirée"
+          border-variant="primary"
+          header-bg-variant="primary"
+          header-text-variant="white"
         >
-          <b-form-input id="input-name" v-model="form.name" placeholder="Nom de votre soirée"></b-form-input>
-          <small id="error-name"></small>
-        </b-form-group>
+          <b-form-group
+            id="input-group-name"
+            label="Quel sera le nom de votre soirée ?"
+            label-for="input-name"
+          >
+            <b-form-input id="input-name" v-model="form.name" placeholder="Nom de votre soirée"></b-form-input>
+            <small id="error-name"></small>
+          </b-form-group>
 
-<b-row>
-  <b-col>
-        <b-form-group
-          id="input-group-date"
-          label="A quelle date se déroulera-t-elle ?"
-          label-for="input-date"
-        >
-          <b-form-datepicker
-            :min="min_date"
-            id="input-date"
-            placeholder="Choisissez une date"
-            v-model="form.date"
-          ></b-form-datepicker>
-          <small id="error-date"></small>
-        </b-form-group>
-  </b-col>
-  <b-col>
-    <b-form-group
-          id="input-group-time"
-          label="A quelle heure se déroulera-t-elle ?"
-          label-for="input-time"
-        >
-    <b-time v-model="form.time" ></b-time>
-    </b-form-group>
-  </b-col>
-</b-row>
+          <b-row>
+            <b-col>
+              <b-form-group
+                id="input-group-date"
+                label="A quelle date se déroulera-t-elle ?"
+                label-for="input-date"
+              >
+                <b-form-datepicker
+                  :min="min_date"
+                  id="input-date"
+                  placeholder="Choisissez une date"
+                  v-model="form.date"
+                ></b-form-datepicker>
+                <small id="error-date"></small>
+              </b-form-group>
+            </b-col>
+            <b-col>
+              <b-form-group
+                id="input-group-time"
+                label="A quelle heure se déroulera-t-elle ?"
+                label-for="input-time"
+              >
+                <b-time v-model="form.time"></b-time>
+              </b-form-group>
+            </b-col>
+          </b-row>
 
-        <b-row align-v="end">
-          <b-col>
-            <b-form-group
-              id="input-group-guest"
-              label="Combien d'invités pourront participer au maximum ?"
-              label-for="input-guest"
-            >
-              <b-form-spinbutton id="input-guest" v-model="form.guest" min="2" max="100"></b-form-spinbutton>
-              <small id="error-guest"></small>
-            </b-form-group>
-          </b-col>
-          <b-col>
-            <b-form-group
-              id="input-group-price"
-              label="Quel sera le prix de participation/personne ?"
-              label-for="input-price"
-            >
-              <b-input-group size="md" prepend="€">
+          <b-row align-v="end">
+            <b-col>
+              <b-form-group
+                id="input-group-guest"
+                label="Combien d'invités pourront participer au maximum ?"
+                label-for="input-guest"
+              >
+                <b-form-spinbutton id="input-guest" v-model="form.guest" min="2" max="100"></b-form-spinbutton>
+                <small id="error-guest"></small>
+              </b-form-group>
+            </b-col>
+            <b-col>
+              <b-form-group
+                id="input-group-price"
+                label="Quel sera le prix de participation/personne ?"
+                label-for="input-price"
+              >
+                <b-input-group size="md" prepend="€">
+                  <b-form-input
+                    id="input-price"
+                    type="number"
+                    v-model="form.price"
+                    min="0"
+                    max="500"
+                    step="0.5"
+                  ></b-form-input>
+                </b-input-group>
+                <small id="error-price"></small>
+              </b-form-group>
+            </b-col>
+          </b-row>
+        </b-card>
+
+        <hr />
+
+        <b-card
+          header="2) Adresse de la soirée"
+          border-variant="info"
+          header-bg-variant="info"
+          header-text-variant="white"
+        >
+          <div class="m-3">
+            <b-button
+              pill
+              variant="outline-success"
+              v-on:click="fillAddress()"
+            >Utiliser l'adresse enregistrée dans mon profil</b-button>
+          </div>
+          <b-row align-v="end">
+            <b-col cols="3">
+              <b-form-group id="input-group-nbAddress">
                 <b-form-input
-                  id="input-price"
+                  id="input-nbAddress"
                   type="number"
-                  v-model="form.price"
-                  min="0"
-                  max="500"
-                  step="0.5"
+                  placeholder="N° de rue"
+                  v-model="form.nbAddress"
+                  step="1"
                 ></b-form-input>
-              </b-input-group>
-              <small id="error-price"></small>
-            </b-form-group>
-          </b-col>
-        </b-row>
+                <small id="error-nbAddress"></small>
+              </b-form-group>
+            </b-col>
+            <b-col cols="9">
+              <b-form-group id="input-group-street">
+                <b-form-input id="input-street" v-model="form.street" placeholder="Nom de rue"></b-form-input>
+                <small id="error-street"></small>
+              </b-form-group>
+            </b-col>
+          </b-row>
+
+          <b-row align-v="end">
+            <b-col>
+              <b-form-group id="input-group-city">
+                <b-form-input id="input-city" type="text" placeholder="Ville" v-model="form.city"></b-form-input>
+                <small id="error-city"></small>
+              </b-form-group>
+            </b-col>
+            <b-col>
+              <b-form-group id="input-group-postCode">
+                <b-form-input
+                  id="input-postCode"
+                  type="number"
+                  v-model="form.postCode"
+                  placeholder="Code postal"
+                  step="1"
+                ></b-form-input>
+                <small id="error-postCode"></small>
+              </b-form-group>
+            </b-col>
+          </b-row>
+        </b-card>
 
         <hr />
 
-        <h4>Adresse de la soirée</h4>
-        <div class="m-3">
-          <b-button
-            pill
-            variant="outline-success"
-            v-on:click="fillAddress()"
-          >Utiliser l'adresse enregistrée dans mon profil</b-button>
-        </div>
-        <b-row align-v="end">
-          <b-col cols="3">
-            <b-form-group id="input-group-nbAddress">
-              <b-form-input
-                id="input-nbAddress"
-                type="number"
-                placeholder="N° de rue"
-                v-model="form.nbAddress"
-                step="1"
-              ></b-form-input>
-              <small id="error-nbAddress"></small>
-            </b-form-group>
-          </b-col>
-          <b-col cols="9">
-            <b-form-group id="input-group-street">
-              <b-form-input id="input-street" v-model="form.street" placeholder="Nom de rue"></b-form-input>
-              <small id="error-street"></small>
-            </b-form-group>
-          </b-col>
-        </b-row>
-
-        <b-row align-v="end">
-          <b-col>
-            <b-form-group id="input-group-city">
-              <b-form-input id="input-city" type="text" placeholder="Ville" v-model="form.city"></b-form-input>
-              <small id="error-city"></small>
-            </b-form-group>
-          </b-col>
-          <b-col>
-            <b-form-group id="input-group-postCode">
-              <b-form-input
-                id="input-postCode"
-                type="number"
-                v-model="form.postCode"
-                placeholder="Code postal"
-                step="1"
-              ></b-form-input>
-              <small id="error-postCode"></small>
-            </b-form-group>
-          </b-col>
-        </b-row>
+        <b-card
+          header="3) Informations complémentaires"
+          border-variant="secondary"
+          header-bg-variant="secondary"
+          header-text-variant="white"
+        >
+          <b-row>
+            <b-col>
+              <b-form-group label="Ajoutez une description" label-for="input-description">
+                <b-form-textarea
+                  id="input-description"
+                  v-model="form.description"
+                  placeholder="Donnez des informations complémentaires sur votre soirée !"
+                  rows="3"
+                  max-rows="6"
+                ></b-form-textarea>
+                <small id="error-description"></small>
+              </b-form-group>
+            </b-col>
+            <b-col>
+              <b-form-group label="Choissisez une image" label-for="input-image">
+                <b-form-file
+                  v-on:input="convertImage()"
+                  v-model="form.image"
+                  id="input-image"
+                  accept=".jpg, .png"
+                  placeholder="(png ou jpeg)"
+                  drop-placeholder="Déposez votre image ici..."
+                ></b-form-file>
+                <small id="error-image"></small>
+              </b-form-group>
+            </b-col>
+          </b-row>
+        </b-card>
 
         <hr />
-
-        <h4>Informations complémentaires</h4>
-
-        <b-row>
-          <b-col>
-            <b-form-group label="Ajoutez une description" label-for="input-description">
-              <b-form-textarea
-                id="input-description"
-                v-model="form.description"
-                placeholder="Donnez des informations complémentaires sur votre soirée !"
-                rows="3"
-                max-rows="6"
-              ></b-form-textarea>
-              <small id="error-description"></small>
-            </b-form-group>
-          </b-col>
-          <b-col>
-            <b-form-group label="Choissisez une image" label-for="input-image">
-              <b-form-file
-                v-on:input="convertImage()"
-                v-model="form.image"
-                id="input-image"
-                accept=".jpg, .png"
-                placeholder="(png ou jpeg)"
-                drop-placeholder="Déposez votre image ici..."
-              ></b-form-file>
-              <small id="error-image"></small>
-            </b-form-group>
-          </b-col>
-        </b-row>
-
-        <hr>
 
         <!-- Hidden input -->
         <input type="hidden" v-model="form.country" />
 
         <b-button type="submit" variant="primary" class="mb-4">Créer la soirée !</b-button>
       </b-form>
-
     </div>
   </div>
 </template>
@@ -221,23 +234,16 @@ export default {
       }
 
       try {
-        let res = await partyService.createParty(this.form)
+        let res = await partyService.createParty(this.form);
 
-        // this.$router.push("/dashboard");
-      } catch (error) {
-        // let err = document.getElementById("error-password");
-        // err.innerText = "Votre identifiant ou mot de passe est incorrect !";
-        // err.classList.add("text-danger");
-      }
+        this.$router.push("/dashboard");
+      } catch (error) {}
     },
 
-
-    
-
     async convertImage() {
-      const file = this.form.image
-      let result =  await toBase64(file)
-      this.form.dataImage = result
+      const file = this.form.image;
+      let result = await toBase64(file);
+      this.form.dataImage = result;
     },
 
     async fillAddress() {
@@ -262,12 +268,14 @@ function getMinDate() {
   return minDate;
 }
 
-async function toBase64(file){ return new Promise((resolve, reject) => {
+async function toBase64(file) {
+  return new Promise((resolve, reject) => {
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = () => resolve(reader.result);
     reader.onerror = error => reject(error);
-})}
+  });
+}
 </script>
 
 <style scoped>
@@ -288,6 +296,10 @@ async function toBase64(file){ return new Promise((resolve, reject) => {
   font-size: 100px;
   color: #35495e;
   letter-spacing: 1px;
+}
+
+.card-header {
+  font-size: 150%;
 }
 
 .subtitle {
--- client/pages/party/part/search.vue
+++ client/pages/party/part/search.vue
@@ -4,25 +4,27 @@
       <h1 class="mt-4">Trouvez vos futures soirées !</h1>
       <hr />
       <b-form inline @submit="onSubmit">
-        <b-form-group label="Sélectionnez votre type d'hôte">
+        <b-form-group class="mr-5">
           <b-form-radio v-model="form.status" name="input-status" value="3">Tout le monde</b-form-radio>
           <b-form-radio v-model="form.status" name="input-status" value="0">Particuliers seulement</b-form-radio>
           <b-form-radio v-model="form.status" name="input-status" value="1">Professionnels seulement</b-form-radio>
         </b-form-group>
 
-        <label for="input-price">Prix max/Personne</label>
-        <b-input-group size="md" prepend="€">
-          <b-form-input
-            id="input-price"
-            type="number"
-            v-model="form.price"
-            min="0"
-            max="500"
-            step="0.5"
-          ></b-form-input>
-        </b-input-group>
+        <div class="mr-5 ml-2 mb-3">
+          <label for="input-price">Prix max/Personne</label>
+          <b-input-group size="md" prepend="€">
+            <b-form-input
+              id="input-price"
+              type="number"
+              v-model="form.price"
+              min="0"
+              max="500"
+              step="0.5"
+            ></b-form-input>
+          </b-input-group>
+        </div>
 
-        <b-form-group id="input-group-date" label="A quelle date ?" label-for="input-date">
+        <b-form-group id="input-group-date" class="mr-5">
           <b-form-datepicker
             :min="min_date"
             id="input-date"
@@ -32,7 +34,7 @@
           ></b-form-datepicker>
         </b-form-group>
 
-        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="submit" variant="primary">Filtrer</b-button>
       </b-form>
       <hr />
 
@@ -119,8 +121,12 @@ export default {
 function decodeImage(data) {
   for (let i = 0; i < data.length; i++) {
     if (data[i].picture != null) {
-      var imgsrc = String.fromCharCode.apply(null, data[i].picture.data);
-      data[i].picture = imgsrc;
+      if (data[i].picture.data.length != 0) {
+        var imgsrc = String.fromCharCode.apply(null, data[i].picture.data);
+        data[i].picture = imgsrc;
+      } else {
+        data[i].picture = "/images/party-food1.jpg";
+      }
     } else {
       data[i].picture = "/images/party-food1.jpg";
     }
