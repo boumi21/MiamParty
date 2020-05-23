14f33ba082478558856da8b917860fd7ef62421c
boumi21 <glorkpatate@yahoo.be>
2020-05-23 18:17:01 +0200

    Augmentation capacité image
    lorsque l'on choisissait une image dans la création d'un soirée, à chaque fois le server disait que c'était trop lourd. -> Corrigé

--- server/app.js
+++ server/app.js
@@ -22,11 +22,12 @@ app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(bodyParser.json())
+app.use(bodyParser.json({limit: '10mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
 
 //JWT middleware
 // app.use(
