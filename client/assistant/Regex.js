9e6a5dfae383107a611f44107f315274b2c9cbfe
boumi21 <glorkpatate@yahoo.be>
2020-06-23 19:47:17 +0200

    Clean assistant

--- client/assistant/Regex.js
+++ client/assistant/Regex.js
@@ -1,26 +1,30 @@
+/*
+    Les regexs utiles sont stockés ici
+*/
+
 function regexEmail(field) {
-    const emailReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    return emailReg.test(field)
+  const emailReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return emailReg.test(field);
 }
 
 function regexPassword(field) {
-    const passwordReg = /^.{6,128}$/
-    return passwordReg.test(field)
+  const passwordReg = /^.{6,128}$/;
+  return passwordReg.test(field);
 }
 
 function regexName(field) {
-    const nameReg = /^[A-Z][A-Za-z\é\è\ê\-]{2,49}$/
-    return nameReg.test(field)
+  const nameReg = /^[A-Z][A-Za-z\é\è\ê\-]{2,49}$/;
+  return nameReg.test(field);
 }
 
 function regexAddress(field) {
-    const addressReg = /^[A-Za-z\é\è\ê\-]{0,49}$/
-    return addressReg.test(field)
+  const addressReg = /^[A-Za-z\é\è\ê\-]{0,49}$/;
+  return addressReg.test(field);
 }
 
 export default {
-    regexEmail,
-    regexPassword,
-    regexName,
-    regexAddress
-}
+  regexEmail,
+  regexPassword,
+  regexName,
+  regexAddress
+};
