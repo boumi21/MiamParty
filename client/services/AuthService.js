460a570e161cbee44e9a7ebbbc41e9ff2110d63f
boumi21 <glorkpatate@yahoo.be>
2020-06-25 11:52:50 +0200

    Clean services

--- client/services/AuthService.js
+++ client/services/AuthService.js
@@ -1,17 +1,16 @@
-import api from "./api"
+import api from "./api";
 
 export default {
-    login(credentials) {
-        return api().post('user/login', credentials)
-    },
-    registerPart(credentials) {
-        return api().post('user/registerPart', credentials)
-    },
-    registerPro(credentials) {
-        return api().post('user/registerPro', credentials)
-    },
-    getCookingLevels(cookingLevels) {
-        console.log("je passe dans authservice")
-        return api().post('part/getCookingLevels', cookingLevels)
-    }
-}
+  login(credentials) {
+    return api().post("user/login", credentials);
+  },
+  registerPart(credentials) {
+    return api().post("user/registerPart", credentials);
+  },
+  registerPro(credentials) {
+    return api().post("user/registerPro", credentials);
+  },
+  getCookingLevels(cookingLevels) {
+    return api().post("part/getCookingLevels", cookingLevels);
+  }
+};
--- client/services/UserService.js
+++ client/services/UserService.js
@@ -1,19 +1,16 @@
-import api from "./api"
+import api from "./api";
 
 export default {
-    
-     getUserAddress(userId) {
-         console.log("je passe dans getUserAddress")
-         return api().post('user/userAddress', userId)
-     },
-     updatePart(credentials) {
-         return api().post('user/updatePart', credentials)
-     },
-     updatePro(credentials) {
-        return api().post('user/updatePro', credentials)
-    },
-     getUserInfo(credentials) {
-         return api().post('user/getUserInfo', credentials)
-     }
-
-}
+  getUserAddress(userId) {
+    return api().post("user/userAddress", userId);
+  },
+  updatePart(credentials) {
+    return api().post("user/updatePart", credentials);
+  },
+  updatePro(credentials) {
+    return api().post("user/updatePro", credentials);
+  },
+  getUserInfo(credentials) {
+    return api().post("user/getUserInfo", credentials);
+  }
+};
