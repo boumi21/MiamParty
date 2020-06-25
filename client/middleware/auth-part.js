ee95e94b3bee65eaa0a01ae29633398ae8535015
boumi21 <glorkpatate@yahoo.be>
2020-06-25 10:55:19 +0200

    clean middleware

--- client/middleware/auth-part.js
+++ client/middleware/auth-part.js
@@ -1,8 +1,12 @@
-export default async function({$auth, redirect}) {
-    let user = $auth.user;
-    if(user && user.isPart){
-        //user in
-    } else {
-        return redirect('/dashboard');
-    }
+/*
+    Middleware à ajouter pour empêcher les particuliers à accéder à une page
+*/
+
+export default async function({ $auth, redirect }) {
+  let user = $auth.user;
+  if (user && user.isPart) {
+    //user in
+  } else {
+    return redirect("/dashboard");
+  }
 }
--- client/middleware/auth-pro.js
+++ client/middleware/auth-pro.js
@@ -1,8 +1,12 @@
-export default async function({$auth, redirect}) {
-    let user = $auth.user;
-    if(user && user.isPro){
-        //user in
-    } else {
-        return redirect('/dashboard');
-    }
+/*
+    Middleware à ajouter pour empêcher les pros à accéder à une page
+*/
+
+export default async function({ $auth, redirect }) {
+  let user = $auth.user;
+  if (user && user.isPro) {
+    //user in
+  } else {
+    return redirect("/dashboard");
+  }
 }
